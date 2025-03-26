f01a42f2a0e34d086fd2995ebe6c99760e87a58e
Rein Krul <info@reinkrul.nl>
2025-03-26 14:39:52 +0100

    permanentIdentifier: only include if explicitly specified

--- credential_issuer/issuer_test.go
+++ credential_issuer/issuer_test.go
@@ -4,10 +4,10 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	ssi "github.com/nuts-foundation/go-did"
 	"github.com/nuts-foundation/go-didx509-toolkit/internal"
 	"testing"
 
-	ssi "github.com/nuts-foundation/go-did"
 	"github.com/nuts-foundation/go-didx509-toolkit/x509_cert"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -78,34 +78,63 @@ func TestIssue(t *testing.T) {
 	validKey, err := internal.ParseRSAPrivateKeyFromPEM([]byte(internal.TestSigningKey))
 	require.NoError(t, err, "failed to parse signing key")
 	t.Run("ok - happy path", func(t *testing.T) {
-		validChain, err := internal.ParseCertificatesFromPEM([]byte(internal.TestCertificateChain))
-		require.NoError(t, err, "failed to parse chain")
+		t.Run("include all", func(t *testing.T) {
+			validChain, err := internal.ParseCertificatesFromPEM([]byte(internal.TestCertificateChain))
+			require.NoError(t, err, "failed to parse chain")
 
-		vc, err := Issue(validChain, validChain[3], validKey, "did:example:123", SubjectAttributes(x509_cert.SubjectTypeCountry, x509_cert.SubjectTypeOrganization, x509_cert.SubjectTypeLocality))
+			vc, err := Issue(validChain, validChain[3], validKey, "did:example:123",
+				SubjectAttributes(x509_cert.SubjectTypeCountry, x509_cert.SubjectTypeOrganization, x509_cert.SubjectTypeLocality),
+				SANAttributes(x509_cert.SanTypeOtherName, x509_cert.SanTypePermanentIdentifierAssigner, x509_cert.SanTypePermanentIdentifierValue),
+			)
 
-		require.NoError(t, err, "failed to issue verifiable credential")
-		require.NotNil(t, vc, "verifiable credential is nil")
+			require.NoError(t, err, "failed to issue verifiable credential")
+			require.NotNil(t, vc, "verifiable credential is nil")
 
-		assert.Equal(t, "https://www.w3.org/2018/credentials/v1", vc.Context[0].String())
-		assert.True(t, vc.IsType(ssi.MustParseURI("VerifiableCredential")))
-		assert.True(t, vc.IsType(ssi.MustParseURI("X509Credential")))
-		assert.Equal(t, "did:x509:0:sha256:DwXSf2_jaUod7cezXBGJBM4AaaoA8DI9j7aPMDTI-mQ::san:otherName:2.16.528.1.1007.99.2110-1-1111111-S-2222222-00.000-333333::subject:L:Testland:O:Faux%20Care", vc.Issuer.String())
+			assert.Equal(t, "https://www.w3.org/2018/credentials/v1", vc.Context[0].String())
+			assert.True(t, vc.IsType(ssi.MustParseURI("VerifiableCredential")))
+			assert.True(t, vc.IsType(ssi.MustParseURI("X509Credential")))
+			assert.Equal(t, "did:x509:0:sha256:DwXSf2_jaUod7cezXBGJBM4AaaoA8DI9j7aPMDTI-mQ::san:otherName:2.16.528.1.1007.99.2110-1-1111111-S-2222222-00.000-333333:permanentIdentifier.assigner:2.16.528.1.1007.3.3:permanentIdentifier.value:2222222::subject:L:Testland:O:Faux%20Care", vc.Issuer.String())
 
-		expectedCredentialSubject := []interface{}{map[string]interface{}{
-			"id": "did:example:123",
-			"subject": map[string]interface{}{
-				"O": "Faux Care",
-				"L": "Testland",
-			},
-			"san": map[string]interface{}{
-				"otherName":                    "2.16.528.1.1007.99.2110-1-1111111-S-2222222-00.000-333333",
-				"permanentIdentifier.assigner": "2.16.528.1.1007.3.3",
-				"permanentIdentifier.value":    "2222222",
-			},
-		}}
+			expectedCredentialSubject := []interface{}{map[string]interface{}{
+				"id": "did:example:123",
+				"subject": map[string]interface{}{
+					"O": "Faux Care",
+					"L": "Testland",
+				},
+				"san": map[string]interface{}{
+					"otherName":                    "2.16.528.1.1007.99.2110-1-1111111-S-2222222-00.000-333333",
+					"permanentIdentifier.assigner": "2.16.528.1.1007.3.3",
+					"permanentIdentifier.value":    "2222222",
+				},
+			}}
 
-		assert.Equal(t, expectedCredentialSubject, vc.CredentialSubject)
-		assert.Equal(t, validChain[0].NotAfter, *vc.ExpirationDate, "expiration date of VC must match signing certificate")
+			assert.Equal(t, expectedCredentialSubject, vc.CredentialSubject)
+			assert.Equal(t, validChain[0].NotAfter, *vc.ExpirationDate, "expiration date of VC must match signing certificate")
+		})
+		t.Run("only include san/otherName", func(t *testing.T) {
+			validChain, err := internal.ParseCertificatesFromPEM([]byte(internal.TestCertificateChain))
+			require.NoError(t, err, "failed to parse chain")
+
+			vc, err := Issue(validChain, validChain[3], validKey, "did:example:123")
+
+			require.NoError(t, err, "failed to issue verifiable credential")
+			require.NotNil(t, vc, "verifiable credential is nil")
+
+			assert.Equal(t, "https://www.w3.org/2018/credentials/v1", vc.Context[0].String())
+			assert.True(t, vc.IsType(ssi.MustParseURI("VerifiableCredential")))
+			assert.True(t, vc.IsType(ssi.MustParseURI("X509Credential")))
+			assert.Equal(t, "did:x509:0:sha256:DwXSf2_jaUod7cezXBGJBM4AaaoA8DI9j7aPMDTI-mQ::san:otherName:2.16.528.1.1007.99.2110-1-1111111-S-2222222-00.000-333333", vc.Issuer.String())
+
+			expectedCredentialSubject := []interface{}{map[string]interface{}{
+				"id": "did:example:123",
+				"san": map[string]interface{}{
+					"otherName": "2.16.528.1.1007.99.2110-1-1111111-S-2222222-00.000-333333",
+				},
+			}}
+
+			assert.Equal(t, expectedCredentialSubject, vc.CredentialSubject)
+			assert.Equal(t, validChain[0].NotAfter, *vc.ExpirationDate)
+		})
 	})
 
 	t.Run("ok - correct escaping of special characters", func(t *testing.T) {
